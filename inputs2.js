02a70014bb50f44c919320161d57533dbe84ea96
d5bf <d5bf@fresnou.org>
2023-02-15 09:51:29 -0800

    commit intake

---
+++ inputs2.js
@@ -0,0 +1,10 @@
+const readLine = require('readline-sync');
+
+
+let question = readLine.question("do you a want to hear a joke?\nYour answer: ");
+console.log(answer);
+
+if (answer1) = ("sure")
+
+let question1 = readLine.question ("deez nuts\nYour answer: ");
+
