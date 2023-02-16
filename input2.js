7cb3b0f4929c6304d4a8b3f848703acb58e04218
d5bf <d5bf@fresnou.org>
2023-02-16 10:14:45 -0800

    commit intake

---
+++ input2.js
@@ -0,0 +1,42 @@
+const readLine = require('readline-sync');
+let num1 = Number(readLine.question("Enter first number: "));
+let num2 = Number(readLine.question("Enter first number: "));
+let sum = num1 + num2;
+console.log(`${num1} + ${num2} = ${sum}`);
+
+
+const readLine = require('readline-sync');
+let num1 = Number(readLine.question("Enter first number: "));
+let num2 = Number(readLine.question("Enter first number: "));
+let sum = num1 - num2;
+console.log(`${num1} - ${num2} = ${sum}`);
+
+
+
+const readLine = require('readline-sync');
+let num1 = Number(readLine.question("Enter first number: "));
+let num2 = Number(readLine.question("Enter first number: "));
+let sum = num1 * num2;
+console.log(`${num1} * ${num2} = ${sum}`);
+
+
+
+const readLine = require('readline-sync');
+let num1 = Number(readLine.question("Enter first number: "));
+let num2 = Number(readLine.question("Enter first number: "));
+let sum = num1 / num2;
+console.log(`${num1} / ${num2} = ${sum}`);
+
+
+
+
+const readLine = require('readline-sync');
+let num1 = Number(readLine.question("Enter first number: "));
+let num2 = Number(readLine.question("Enter first number: "));
+let sum = num1 % num2;
+console.log(`${num1} % ${num2} = ${sum}`);
+
+
+
+
+
