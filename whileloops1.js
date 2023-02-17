706f7e4d5844b0ed16f132762516732ddc9289d0
d5bf <d5bf@fresnou.org>
2023-02-17 10:28:59 -0800

    commit intake

---
+++ whileloops1.js
@@ -0,0 +1,30 @@
+const readLine = require('readline-sync');
+
+
+
+let start = 0;
+
+while (start <= 26) {
+
+    if (start < 10) {
+
+        console.log(start);
+
+    } else if (start > 25) {
+
+        console.log(start);
+
+    }
+}
+
+let x = 0;
+
+
+while (x < 26){
+
+console.log(x);
+
+x++;
+
+}
+
